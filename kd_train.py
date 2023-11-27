953b018cd142a753e2e9da2c29c18dd4b878111a
chu0802 <r09922104@ntu.edu.tw>
2023-11-27 12:50:05 +0800

    <feat>: add a script for kd training

---
+++ kd_train.py
@@ -0,0 +1,41 @@
+import os
+
+from src.utils.config import get_config
+from src.tasks import get_task
+import logging
+import sys
+from src.models import get_model
+from src.evaluater import get_evaluater
+from src.datasets.utils import get_dataloaders
+from src.utils.seed import setup_seeds
+from src.trainer.trainer import Trainer
+from src.utils.wandb import wandb_logger
+
+log_format = """[%(levelname)s] [%(asctime)s] %(message)s"""
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_format)
+
+
+@wandb_logger
+def main(config):
+    setup_seeds(config.task.seed)
+
+    logging.info("Start Inference")
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(config.task.device)
+
+    task = get_task(config)
+    dataloaders = get_dataloaders(config)
+    evaluater = get_evaluater(config.dataset.name)
+    model = get_model(config)
+
+    trainer = Trainer(task, model, dataloaders, config)
+
+    trainer.train()
+    score = trainer.evaluate(evaluater)
+
+    logging.info(
+        f"Model: {config.model.name} | Dataset: {config.dataset.name} | Result: {score}"
+    )
+
+
+if __name__ == "__main__":
+    main(get_config(mode="train"))
